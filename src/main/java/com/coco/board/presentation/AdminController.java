3ea90b28aedbea12684c27ab20e0860e389d81eb
Uguls <chobozaex@gmail.com>
2023-05-16 19:43:56 +0900

    어드민 페이지 수정

--- src/main/java/com/coco/board/presentation/AdminController.java
+++ src/main/java/com/coco/board/presentation/AdminController.java
@@ -2,6 +2,7 @@ package com.coco.board.presentation;
 
 import com.coco.board.application.AdminService;
 import com.coco.board.application.dto.UserDto;
+import com.coco.board.application.security.auth.LoginUser;
 import com.coco.board.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -17,7 +18,12 @@ public class AdminController {
     private final AdminService adminService;
 
     @GetMapping("/manage")
-    public String getUsers(Model model) {
+    public String getUsers(Model model, @LoginUser UserDto.Response user) {
+
+        if (user != null) {
+            model.addAttribute("user", user);
+        }
+
         List<User> users = adminService.getUsers();
         model.addAttribute("users", users);
         return "admin/user-list";
